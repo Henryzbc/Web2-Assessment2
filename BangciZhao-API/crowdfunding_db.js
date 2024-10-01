cebb5f9944a74d0687fea917c8489a88d69783b6
Henryzbc <3289435499@qq.com>
2024-10-01 16:53:10 +0800

    Modifies the static resource path

--- BangciZhao-API/crowdfunding_db.js
+++ BangciZhao-API/crowdfunding_db.js
@@ -3,9 +3,9 @@ const require = createRequire(import.meta.url);
 const mysql = require('mysql')
 const db = mysql.createConnection({
     user: 'root',
-    password: 'admin',
+    password: '123456',
     host: 'localhost',
-    database: 'crowdfundingcr'
+    database: 'crowdfunding_db'
 })
 
 export default db
