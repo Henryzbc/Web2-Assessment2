82e77e7ebdba59016f113ba1cda250139301d396
Henryzbc <3289435499@qq.com>
2024-10-01 16:29:17 +0800

    change api port

--- BangciZhao-API/index.js
+++ BangciZhao-API/index.js
@@ -73,6 +73,6 @@ app.get('/category', (req, res) => {
     });
 });
 
-app.listen(8080, () => {
-    console.log('Server is running on port 8080.');
+app.listen(4000, () => {
+    console.log('Server is running on port 4000.');
 });
